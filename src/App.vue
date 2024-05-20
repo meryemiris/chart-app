80834705b1bc814ae78d903a1f594fed274b377d
Meryem İris <irismeryem.96@gmail.com>
2024-05-20 17:50:48 +0300

    install tailwindcss, and add headers

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,22 @@ import Chart from "./components/Chart.vue";
 </script>
 
 <template>
-	<header></header>
+	<div class="flex flex-col items-center">
+		<header
+			class="flex mt-[38px] w-[412.5px] flex-col gap-3 text-center font-sans font-semibold text-2xl"
+		>
+			<h1 class="text-primary">Marketing Integrations</h1>
+			<h2 class="text-secondary">Trust WASK's smart optimization features</h2>
+		</header>
 
-	<main>
-		<Chart />
-	</main>
+		<main>
+			<Chart />
+		</main>
+	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	theme: {
+		fontFamily: {
+			sans: ["Inter", "sans-serif"],
+		},
+		extend: {
+			colors: {
+				primary: "#131313",
+				secondary: "#D0D5DD",
+			},
+		},
+	},
+	plugins: [],
+};
