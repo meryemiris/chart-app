17b48bd0edefba9ca972018324f10a3ece76dd22
Meryem İris <irismeryem.96@gmail.com>
2024-05-20 16:29:10 +0300

    fetch data, install chartjs and create simple chart

---
+++ src/components/Chart.vue
@@ -0,0 +1,101 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { Chart, registerables } from "chart.js";
+
+Chart.register(...registerables);
+
+const data = ref(null);
+const error = ref(null);
+const isLoading = ref(false);
+
+const selectedAccount = ref(0);
+
+const chartLabels = ref([]);
+const spendData = ref([]);
+const clicksData = ref([]);
+const impressionsData = ref([]);
+
+const fetchData = async () => {
+	try {
+		isLoading.value = true;
+		const response = await fetch(import.meta.env.VITE_BASE_URL, {
+			method: "GET",
+			headers: {
+				token: import.meta.env.VITE_ACCESS_TOKEN,
+			},
+		});
+		if (!response.ok) {
+			throw new Error("Failed to fetch data");
+		}
+		const result = await response.json();
+		data.value = result;
+
+		createChart();
+	} catch (err) {
+		error.value = err.message;
+	} finally {
+		isLoading.value = false;
+	}
+};
+
+const createChart = () => {
+	const insightsData = data.value.accounts[selectedAccount.value].insights.data;
+
+	chartLabels.value = insightsData.map((item) => item.date_start);
+	spendData.value = insightsData.map((item) => parseFloat(item.spend));
+	clicksData.value = insightsData.map((item) => parseInt(item.clicks));
+	impressionsData.value = insightsData.map((item) =>
+		parseInt(item.impressions)
+	);
+	const ctx = document.getElementById("myChart").getContext("2d");
+	new Chart(ctx, {
+		type: "line",
+		data: {
+			labels: chartLabels.value,
+			datasets: [
+				{
+					label: "Spend",
+					data: spendData.value,
+					borderColor: "rgb(75, 192, 192)",
+					backgroundColor: "rgba(75, 192, 192, 0.2)",
+					borderWidth: 1,
+				},
+				{
+					label: "Clicks",
+					data: clicksData.value,
+					borderColor: "rgb(54, 162, 235)",
+					backgroundColor: "rgba(54, 162, 235, 0.2)",
+					borderWidth: 1,
+				},
+				{
+					label: "Impressions",
+					data: impressionsData.value,
+					borderColor: "rgb(255, 206, 86)",
+					backgroundColor: "rgba(255, 206, 86, 0.2)",
+					borderWidth: 1,
+				},
+			],
+		},
+		options: {
+			responsive: true,
+			scales: {
+				y: {
+					beginAtZero: true,
+				},
+			},
+		},
+	});
+};
+
+onMounted(() => {
+	fetchData();
+});
+</script>
+
+<template>
+	<canvas id="myChart" width="400" height="400"></canvas>
+	<div v-if="isLoading">Loading...</div>
+	<div v-if="error">{{ error }}</div>
+</template>
+
+<style scoped></style>
