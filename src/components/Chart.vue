bb6c98041ac9a8f5cbe3d9cbd56c97dab44bf4c4
Meryem İris <irismeryem.96@gmail.com>
2024-05-22 12:28:43 +0300

    install chartjs, use vue-chartjs wrapper, and rewrite chart component

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,152 +1,99 @@
 <script setup>
-import { ref, onMounted, watch } from "vue";
-import * as echarts from "echarts/core";
+import { Line } from "vue-chartjs";
 import {
-	TitleComponent,
-	ToolboxComponent,
-	TooltipComponent,
-	GridComponent,
-	LegendComponent,
-} from "echarts/components";
-import { LineChart } from "echarts/charts";
-import { CanvasRenderer } from "echarts/renderers";
+	Chart as ChartJS,
+	Title,
+	Tooltip,
+	Legend,
+	LineElement,
+	PointElement,
+	CategoryScale,
+	LinearScale,
+} from "chart.js";
+import { computed, reactive } from "vue";
 
-echarts.use([
-	TitleComponent,
-	ToolboxComponent,
-	TooltipComponent,
-	GridComponent,
-	LegendComponent,
-	LineChart,
-	CanvasRenderer,
-]);
+ChartJS.register(
+	Title,
+	Tooltip,
+	Legend,
+	LineElement,
+	PointElement,
+	CategoryScale,
+	LinearScale
+);
 
 const props = defineProps({
-	data: Array,
+	data: {
+		type: Object,
+		required: true,
+	},
+	options: {
+		type: Object,
+		default: () => ({}),
+	},
+	metric: {
+		type: String,
+	},
 });
 
-const chartInstance = ref(null);
-
-const createChart = () => {
-	const chartDom = document.getElementById("main");
-	if (chartInstance.value) {
-		chartInstance.value.dispose(); // Dispose existing chart instance if any
-	}
-	chartInstance.value = echarts.init(chartDom);
-
-	const chartLabels = props.data.map((item) => item.date_start);
-	const spendData = props.data.map((item) => parseFloat(item.spend));
-	const clicksData = props.data.map((item) => parseInt(item.clicks));
-	const impressionsData = props.data.map((item) => parseInt(item.impressions));
-
-	const option = {
-		// title: {
-		// 	text: "Chart Data",
-		// },
-		tooltip: {
-			trigger: "axis",
-			axisPointer: {
-				type: "cross",
-				label: {
-					backgroundColor: "rgba(0, 0, 0, 0.8)",
-				},
-			},
+// Data and Options
+const chartData = computed(() => ({
+	labels: props.data.dates,
+	datasets: [
+		{
+			label: props.metric,
+			backgroundColor: "rgba(75, 192, 192, 0.2)",
+			borderColor: "rgba(75, 192, 192, 1)",
+			borderWidth: 5,
+			pointBackgroundColor: "rgba(75, 192, 192, 1)",
+			pointBorderColor: "#fff",
+			pointHoverBackgroundColor: "#fff",
+			pointHoverBorderColor: "rgba(75, 192, 192, 1)",
+			data: props.data.values,
 		},
+	],
+}));
+
+const chartOptions = computed(() => ({
+	responsive: true,
+	plugins: {
 		legend: {
-			data: ["Spend", "Clicks", "Impressions"],
+			position: "top",
 		},
-		toolbox: {
-			feature: {
-				saveAsImage: {},
-				// dataView: { readOnly: false },
-			},
+		title: {
+			display: true,
+			text: "Chart.js Line Chart",
 		},
-		grid: {
-			left: "3%",
-			right: "4%",
-			bottom: "3%",
-			containLabel: true,
+	},
+	scales: {
+		x: {
+			beginAtZero: true,
 		},
-		xAxis: {
-			type: "category",
-			boundaryGap: false,
-			data: chartLabels,
+		y: {
+			beginAtZero: true,
 		},
-		yAxis: [
-			{
-				type: "value",
-				name: "Spend/Clicks",
-				position: "left",
-			},
-			{
-				type: "value",
-				name: "Impressions",
-				position: "right",
-			},
-		],
-		series: [
-			{
-				name: "Spend",
-				type: "line",
-				stack: "Total",
-				data: spendData,
-				emphasis: {
-					focus: "series",
-				},
-				areaStyle: {},
-				yAxisIndex: 0,
-			},
-			{
-				name: "Clicks",
-				type: "line",
-				stack: "Total",
-				data: clicksData,
-				emphasis: {
-					focus: "series",
-				},
-				areaStyle: {},
-				yAxisIndex: 0,
-			},
-			{
-				name: "Impressions",
-				type: "line",
-				stack: "Total",
-				data: impressionsData,
-				emphasis: {
-					focus: "series",
-				},
-				areaStyle: {},
-				yAxisIndex: 1,
-			},
-		],
-	};
+	},
+}));
 
-	chartInstance.value.setOption(option);
-};
-
-onMounted(createChart);
-
-watch(
-	() => props.data,
-	(newData) => {
-		if (newData && newData.length) {
-			createChart();
-		}
-	}
-);
+const myStyles = reactive({
+	height: "423px",
+	width: "338px",
+	backgroundColor: "white",
+	position: "relative",
+});
 </script>
 
 <template>
 	<div
-		class="flex items-center justify-center bg-[#F9FAFB] flex-auto m-[33px] lg:w-[956px] lg:h-[537px] rounded-[4.5px]"
+		class="flex items-center justify-center bg-[#F9FAFB] w-[371px] h-[452px] rounded-[4.5px] ml-[33px]"
 	>
-		<div
-			class="relative flex justify-center items-center p-[33px] h-[452px] w-[371px] lg:w-[897px] lg:h-[481px]"
-		>
-			<div
-				id="main"
-				class="w-[338px] h-[423px] lg:w-[897px] lg:h-[481px]"
-			></div>
+		<div>
+			<Line
+				v-if="chartData && chartOptions"
+				:data="chartData"
+				:options="chartOptions"
+				:style="myStyles"
+			/>
 		</div>
 	</div>
 </template>
